682c96a7df8f944af5c1b3c969a19c890591b95c
rosselgalarzarafael <rosselgalarzarafael@gmail.com>
2024-10-24 00:57:46 -0500

    api terminada, con get, delete, put , post

--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,5 +1,5 @@
 export class CreateProductDTO {
-
+   
    readonly name :string; // el readonly lo usariamos para trabajar con datos inmutables
    readonly  description : string ;
    readonly  imgaeURL : string;
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Post, Get, Delete, Put, Res, HttpStatus, Body, Param } from '@nestjs/common';
+import { Controller, Post, Get, Delete, Put, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
 
 import { CreateProductDTO } from './dto/product.dto';
 import { ProductService } from './product.service'
 
+
 @Controller('product')
 export class ProductController {
-    constructor(private productService: ProductService){}
+    constructor(private productService: ProductService) { }
 
     @Post('/create')
     async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
-      const product= await  this.productService.createProduct(createProductDTO)
+        const product = await this.productService.createProduct(createProductDTO)
         return res.status(HttpStatus.OK).json({
             message: 'product successfully create',
             product: product
@@ -17,18 +18,50 @@ export class ProductController {
     }
 
     @Get('/')
-    async getProducts(@Res() res){
+    async getProducts(@Res() res) {
         const products = await this.productService.getProducts();
         return res.status(HttpStatus.OK).json({
-             products
-         })
+            products
+        })
     }
-  @Get('/:productID')
-  async getProduct(@Res() res, @Param('productID') productID){
+    @Get('/:productID')
+    async getProduct(@Res() res, @Param('productID') productID) {
 
-    const product = await this.productService.getProduct(productID);
-    return res.status(HttpStatus.OK).json(product)
+        const product = await this.productService.getProduct(productID);
+        if (!product) throw new NotFoundException('Producto no encontrado');
+        return res.status(HttpStatus.OK).json(product);
 
-  }
+    }
+
+    //delete de una manera pasando el productId
+
+    @Delete('/delete/:productID')
+    async deleteProduct(@Res() res, @Param('productID') productID) {
+
+        const product = await this.productService.deleteProduct(productID);
+        if (!product) throw new NotFoundException('Producto no encontrado');
+        return res.status(HttpStatus.OK).json(product);
+    }
+    //segunda forma de eliminar un producto con product/delete?productID=3b334abc56cccb4sc75b5da
+    @Delete('/delete')
+    async deleteProduct2(@Res() res, @Query('productID') productID) {
+        const productDelete = await this.productService.deleteProduct(productID)
+        if (!productDelete) throw new NotFoundException('Producto no encontrado');
+
+        return res.status(HttpStatus.OK).json({
+            message: 'producto eliminado',
+            productDelete: productDelete
+        })
+    }
+
+    @Put('/update')
+   async  updateProduct(@Res() res , @Body()  createProductDTO : CreateProductDTO , @Query('productID') productID ){
+       const productUpdate = await this.productService.updateProduct(productID, createProductDTO)
+       if (!productUpdate) throw new NotFoundException('Producto no encontrado');
+        return res.status(HttpStatus.OK).json({
+            message: 'Producto actualizado',
+             productUpdate
+        })
+    }
 
 }
